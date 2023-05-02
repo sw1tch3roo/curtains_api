ee2bd6b188458e61246329fe1f7c21f6c2c1aadd
Dmitry <chi3fno1d@gmail.com>
2023-05-02 16:23:54 +0300

    addControllers

---
+++ controllers/typeController.js
@@ -0,0 +1,14 @@
+const ApiError = require('../error/ApiError');
+const { Type } = require('../models/models');
+
+class typeController {
+  async create(req, res) {
+    const { name } = req.body;
+    const type = await Type.create({ name });
+    return res.json(type);
+  }
+
+  async getAll(req, res) {}
+}
+
+module.exports = new typeController();
